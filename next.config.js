144dd64d197b3c705fb8bccea8bc2554a943587a
hoangVu.dev <hoangk62uet@gmail.com>
2023-06-14 22:48:50 +0700

    implement project

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+      {
+        source: "/index",
+        destination: "/_index",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
